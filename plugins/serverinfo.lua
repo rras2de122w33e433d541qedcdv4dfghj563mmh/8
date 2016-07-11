e2eda613f62a5af945cd5b1449ac05bec758ba8a
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-11 04:25:07 -0700

    Update serverinfo.lua

--- plugins/serverinfo.lua
+++ plugins/serverinfo.lua
@@ -1,5 +1,5 @@
 local function run(msg, matches)
-local text = io.popen("sh ./data/cmd.sh"):read('*all')
+local text = io.popen("sh ./Turbo/data/cmd.sh"):read('*all')
 if is_sudo(msg) then
   return text
 end
