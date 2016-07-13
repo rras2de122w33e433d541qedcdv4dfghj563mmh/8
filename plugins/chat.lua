ab4071fcbcd299f4203d04184e1be96bf6f28180
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-13 11:12:15 -0700

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
   local reply_id = msg['id']
   local text = matches[1]
-  local key = 2c197815-21ba-42dc-b20c-c647a49cd963
+  local key =2c197815-21ba-42dc-b20c-c647a49cd963
   local url = 'http://sandbox.api.simsimi.com/request.p?key='..key..'&text='..text..'&lc=fa&ft=0.0'
   local jstr = http.request(url)
   local jdat = json:decode(jstr)
