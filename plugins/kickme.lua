6076e551743c55c46d31f4d95defbbddfe1dc4a3
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-16 11:28:48 -0700

    Update kickme.lua

--- plugins/kickme.lua
+++ plugins/kickme.lua
@@ -2,7 +2,7 @@ local function run(msg, matches)
 if matches[1] == 'kickme' then
 local hash = 'kick:'..msg.to.id..':'..msg.from.id
      redis:set(hash, "waite")
-      return '🔖کاربر عزیز ('..msg.from.username..')\nشما درخواست اخراج خود از گروه را ارسال کردید\nاگر با این درخواست موافقت دارید عبارت yes را ارسال کنید'
+      return 'Dear User :  ['..msg.from.username..']\nIf You want to go Please Type : <code>yes</code>\nand if you want to Stay Type : <code>no</code>'
     end
 
     if msg.text then
@@ -10,7 +10,7 @@ local hash = 'kick:'..msg.to.id..':'..msg.from.id
       if msg.text:match("^yes$") and redis:get(hash) == "waite" then
 	  redis:set(hash, "ok")
 	elseif msg.text:match("^no$") and redis:get(hash) == "waite" then
-	send_large_msg(get_receiver(msg), "کرم داری ؟ ")
+	send_large_msg(get_receiver(msg), "Kickme Status : <code>No</code>\nYou are in !")
 	  redis:del(hash, true)
 
       end
@@ -19,7 +19,7 @@ local hash = 'kick:'..msg.to.id..':'..msg.from.id
 	 if redis:get(hash) then
         if redis:get(hash) == "ok" then
          channel_kick("channel#id"..msg.to.id, "user#id"..msg.from.id, ok_cb, false)
-         return '❌کاربر مورد نظر بنابر درخواست خود از گروه ('..msg.to.title..') اخراج شد'
+         return 'Kickme Status : <code>Yes</code>\nUser <code>('..msg.to.title..')</code> Have Been Kicked !'
         end
       end
     end
