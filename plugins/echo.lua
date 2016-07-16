b943c8e2e71f4e6ea4845cbb807c1a39809b55cd
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-16 09:46:42 -0700

    Create echo.lua

---
+++ plugins/echo.lua
@@ -0,0 +1,22 @@
+local function run(msg, matches)
+      if not is_owner(msg) then
+    return
+end
+  local text = '<code>'..matches[1]..'</code>'
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  return text
+end
+
+return {
+  description = "Simplest plugin ever!",
+  usage = "!echo [whatever]: echoes the msg",
+  patterns = {
+    "^[/#!]echo +(.+)$"
+  }, 
+  run = run 
+}
