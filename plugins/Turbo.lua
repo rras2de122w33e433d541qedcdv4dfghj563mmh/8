35c929465e210d4091e4533d95e97ad9f22be888
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-24 10:27:59 -0700

    Update Turbo.lua

--- plugins/Turbo.lua
+++ plugins/Turbo.lua
@@ -26,13 +26,14 @@ local info = io.popen("sh ./Turbo/sev.sh"):read('*all')
 local our = 'yeo.ir/TurboBoT'
 local Channel = 'Soon!'
 local sup = '@supturbobot'
-   local data = load_data(_config.moderation.data)
-   local group_link = data[tostring(1045086781)]['settings']['set_link'] --put your support id here
-   send_document("channel#id"..msg.to.id,"./Turbo/rank/jojo.webp", ok_cb, false)
+	local data = load_data(_config.moderation.data)
+	local settings = data[tostring(1045086781)]['settings']
+	local group_link = data[tostring(1045086781)]['settings']['set_link'] --put your support id here
+	if not group_link then
+		group_link = "[none]"
+	end
 
-local text = ""..space.."\nTurbo Anti Spam BoT V5⃣\n"..space.."\n    \nDevelopers & The builders and the owner of the rating :\n@ArmanTurbo\n@Xx_AE_xX\n"..admins.."\n\n🙏Special thanks to :\nArman-Turbo [Developer] & [Manager] & [Founder]\nAmir-viper [Developer] & [designer]\nDead [designer] & [Admin]\nAnd All My Friends\n\n🔆 Turbo ™ Technical specifications of the server :\n\nuptime : "..rates.." days"..rates1.." hours\n\n"..info..""..space.."\nCheckout :\n"..our.."\n"..space.."\nBridges of communication :\n\n📢 Channel : "..Channel.."\nSupport BoT :\n"..sup.."\n📝 Please send your feedback\nThe command Feedback [text]\nSupport link :\n"..group_link or none
-if not group_link then
-end
+local text = ""..space.."\nTurbo Anti Spam BoT V5⃣\n"..space.."\n    \nDevelopers & The builders and the owner of the rating :\n@ArmanTurbo\n@Xx_AE_xX\n"..admins.."\n\n🙏Special thanks to :\nArman-Turbo [Developer] & [Manager] & [Founder]\nAmir-viper [Developer] & [designer]\nDead [designer] & [Admin]\nAnd All My Friends\n\n🔆 Turbo ™ Technical specifications of the server :\n\nuptime : "..rates.." days"..rates1.." hours\n\n"..info..""..space.."\nCheckout :\n"..our.."\n"..space.."\nBridges of communication :\n\n📢 Channel : "..Channel.."\nSupport BoT :\n"..sup.."\n📝 Please send your feedback\nThe command Feedback [text]\nSupport link :\n"..group_link
 return text
 end
 return {
